4a14c2998e2c3acb9edc2960a87a64385aa37a12
giedriusl <giedrius.lukocius@gmail.com>
2019-11-04 23:33:14 +0200

    create unfinished menu

---
+++ Assets/Scripts/OptionsPanel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsPanel : MonoBehaviour
+{
+    public Button optionsButton;
+
+    [SerializeField]
+    private GameObject[] panels;
+
+    void OnEnable()
+    {
+        //Register Button Events
+        optionsButton.onClick.AddListener(() => buttonCallBack(panels));
+    }
+
+    private void buttonCallBack(GameObject[] panels)
+    {
+        panels[0].SetActive(true);
+        panels[1].SetActive(false);
+    }
+
+    void OnDisable()
+    {
+        //Un-Register Button Events
+        optionsButton.onClick.RemoveAllListeners();
+    }
+}
